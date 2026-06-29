e258f8549b060697ce3584faeef0ffc6fb6f2173
Luthfi Fauzi <luthfafi2nd@gmail.com>
2026-06-29 11:00:03 +0700

    cleanup: remove DummyJobPostingSeeder and disable dummy data generation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -36,8 +36,5 @@ class DatabaseSeeder extends Seeder
 
         // Seed default scraper sources
         $this->call(ScraperSourceSeeder::class);
-
-        // Seed dummy job postings for explore view testing
-        $this->call(DummyJobPostingSeeder::class);
     }
 }
